d12bec1246c32d1f68b9e04c00074c34434cc993
Rosario Garufi <saro.garufi1@gmail.com>
2022-05-25 16:01:43 +0200

    Consegna esercizio

--- index.php
+++ index.php
@@ -6,4 +6,42 @@
 
  <?php
 
- 
+ class movie{
+     public $title;
+     public $category;
+     public $minutes;
+     public $year_release;
+
+     function __costructor(String $title ,String $category,String $minutes,String $year_release){
+         $this->title = $title;
+         $this->category = $category;
+         $this->minutes = $minutes;
+         $this->year_release = $year_release;
+     }
+     
+     /**
+      * @return title, title of the film;
+      
+        
+      */
+     public function get_title(){
+         return $this->title;
+     }
+     public function get_category(){
+        return $this->category;
+    }
+    public function get_minutes(){
+        return $this->minutes;
+    }
+    public function get_year_release(){
+        return $this->year_release;
+    }
+ };
+
+ $film= new Movie('Avengers', 'Fantascienza', '90', '2018');
+ $film ->title = 'avengers';
+$film ->category = 'fantascienza';
+$film ->minutes = '120';
+$film ->year_release = '2018';
+ var_dump($film);
+
